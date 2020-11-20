8a1b2ff899f104dd907bc7c8a7f9742d536c5e6a
Sarah Damag <sarahdamag@gmail.com>
2020-11-20 22:58:56 +0100

    with NavBar component

--- projet3-client/src/App.js
+++ projet3-client/src/App.js
@@ -1,35 +1,36 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import Nav from "./components/Nav";
+import Nav from './components/Nav';
 import { Switch, Route } from 'react-router-dom';
-import Signup from './components/Signup'
+import Signup from './components/Signup';
 
 class App extends React.Component {
+   state = { loggedInUser: null };
 
-  state = { loggedInUser: null }
+   updateLoggedInUser = (newUser) => {
+      this.setState({
+         loggedInUser: newUser,
+      });
+   };
 
-  updateLoggedInUser=(newUser)=>{
-    this.setState ({
-      loggedInUser: newUser
-    })
-  }
-
-  render(){
-    return (
-      <div className="App">
-      <Signup></Signup>
-
-      Coucou
-          <Switch>
-            {/* <Route exact path="/" component={Home}/> */}
-            <Route exact path="/signup" render={()=> <Signup updateUser={this.updateLoggedInUser}/>}/>
-            {/* <Route exact path="/login" component={Login}/> */}
-          </Switch>
-      </div>
-    );
-
-  }
-  
+   render() {
+      return (
+         <div className='App'>
+            <Nav />
+            <Signup></Signup>
+            Coucou
+            <Switch>
+               {/* <Route exact path="/" component={Home}/> */}
+               <Route
+                  exact
+                  path='/signup'
+                  render={() => <Signup updateUser={this.updateLoggedInUser} />}
+               />
+               {/* <Route exact path="/login" component={Login}/> */}
+            </Switch>
+         </div>
+      );
+   }
 }
 
 export default App;
--- projet3-client/src/components/Nav.js
+++ projet3-client/src/components/Nav.js
@@ -1,25 +1,48 @@
-import React from "react";
-import {Link} from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
 
 class Nav extends React.Component {
-
-  render() {
-    return(
-
-      <div className="nav">
-        
-        <Link to="/login">LOGIN</Link>
-
-      </div>
-
-     
-
-
-    )
-  }
-
-
+   render() {
+      return (
+         <div className='nav'>
+            <Link className='link' to='/'>
+               <img src='../images/logo_aadh.png' alt='logo_aadh' />
+            </Link>
+            <div className='nav-row'>
+               <ul className='nav-row nav-menu'>
+                  <li>
+                     <Link className='link' to='/missions'>
+                        Toutes les missions
+                     </Link>
+                  </li>
+                  <li>
+                     <Link className='link' to='/missions'>
+                        Publier une mission
+                     </Link>
+                  </li>
+                  <li>
+                     <Link className='link' to='/'>
+                        Actualités
+                     </Link>
+                  </li>
+                  <li>
+                     <Link className='link' to='/'>
+                        Rejoignez-nous
+                     </Link>
+                  </li>
+               </ul>
+               <div className='user_button'>
+                  <FaUserCircle className='user_icon'></FaUserCircle>
+                  <div className='user_links'>
+                     <Link to={'/loggedin'}>Mon espace</Link>
+                     <Link to={'/signup'}>S'inscrire</Link>
+                  </div>
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default Nav;
