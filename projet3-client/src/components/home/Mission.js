97e9efece9c57280f26b6306cea0bf16ea83d62f
Sarah Damag <sarahdamag@gmail.com>
2020-11-26 14:11:45 +0100

    with home finished

---
+++ projet3-client/src/components/home/Mission.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Mission = () => {
+   return (
+      <div>
+         « L’AADH coordonne la fourniture de services juridiques gratuits et confidentiels auprès
+         des ONG, associations et institutions dédiées à la protection des droits humains, quelles
+         que soient leurs dimensions nationales ou internationales (pourvu qu’elles aient été agrées
+         préalablement par le conseil d’administration), et des victimes de violations de leurs
+         droits fondamentaux. Les avocats des cabinets membres, représentant plus de 26 000 avocats
+         bénévoles, répondent à l’ensemble des problématiques juridiques rencontrées par ces
+         organisations essentiellement sous forme de consultations et de conseils. » Ces avocats,
+         représentés par 42 cabinets d’avocats adhérents, s’engagent auprès de l’AADH volontairement
+         et gracieusement. Ils sont présents dans le monde entier: via des cabinets français ou
+         internationaux avec une filiale en France (Paris ou province) à dominante droit des
+         affaires via des cabinets français à dominante droit pénal (français et international)
+      </div>
+   );
+};
+export default Mission;
