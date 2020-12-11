05ddb76ef779b2551d0faf11ab70329fdea20b0d
Sarah Damag <sarahdamag@gmail.com>
2020-12-11 12:44:29 +0100

    with edit user done

--- projet3-client/src/components/missions/MissionDetails.js
+++ projet3-client/src/components/missions/MissionDetails.js
@@ -1,118 +1,140 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { GrLocation } from "react-icons/gr";
-import service from "../auth-service";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { GrLocation } from 'react-icons/gr';
+import service from '../auth-service';
+// import moment from 'moment';
+
+import EditMission from './EditMission.js';
 
 class MissionDetails extends React.Component {
-  state = {};
+   state = {};
 
-  componentDidMount() {
-    this.getSingleMission();
-  }
+   componentDidMount() {
+      this.getSingleMission();
+   }
 
-  getSingleMission = () => {
-    const { params } = this.props.match;
-    service
-      .get(`/missions/${params.id}`)
-      .then((responseFromApi) => {
-        const theMission = responseFromApi.data;
-        this.setState(theMission);
-        this.setState({
-          start_date: this.formatDate(this.state.start_date),
-          end_date: this.formatDate(this.state.end_date),
-        });
-        //console.log("themission to check date from mission details", theMission)
-        console.log(
-          "this.state of MissionDetails formatted",
-          this.state.start_date
-        );
-      })
-      .catch((err) => {
-        console.log("Error while fetching mission", err);
-      });
-  };
+   getSingleMission = () => {
+      const { params } = this.props.match;
+      service
+         .get(`/missions/${params.id}`)
+         .then((responseFromApi) => {
+            const theMission = responseFromApi.data;
+            this.setState(theMission);
+         })
+         .catch((err) => {
+            console.log('Error while fetching mission', err);
+         });
+   };
 
-  //instead of recalling getAllMissions in the setState above, check both states then display:
-  // componentDidUpdate(prevProps, prevState) {
-  //   console.log("just re-rendered", prevProps, prevState);
-  //   if (([this.state] != [prevState])) {
-  //     console.log("state missiondetails changed", prevState, this.state);
-  //     this.getSingleMission();
-  //   }
-  // }
+   renderEditForm = () => {
+      if (!this.state.title) {
+         this.getSingleMission();
+      } else {
+         return (
+            <EditMission
+               theMission={this.state}
+               getTheMission={this.getSingleMission}
+               {...this.props}
+            />
+         );
+      }
+   };
 
-  //error message: The specified value "2020-12-25T00:00:00.000Z" does not conform to the required format, "yyyy-MM-dd"
-  // I want to display only YYYY/MM/DD
-  // Method 1 - using npm moment:
-  // const date = moment("Sep 21, 2017").format('YYYY/MM/DD');
-  // console.log(date); //2017/09/21
+   // DELETE MISSION
+   deleteMission = () => {
+      const { params } = this.props.match;
+      service
+         .delete(`/missions/${params.id}`)
+         .then(() => {
+            this.props.history.push('/missions');
+         })
+         .catch((err) => {
+            console.log('Error while deleting mission', err);
+         });
+   };
 
-  // Method 2 - using substring
-  // var finalDate = "2013-03-10T02:00:00Z";
-  // finalDate.substring(0, 10);
-  //returns "2013-03-10"
+   //error message: The specified value "2020-12-25T00:00:00.000Z" does not conform to the required format, "yyyy-MM-dd"
+   // I cant to display only YYYY/MM/DD
+   // Method 1 - using npm moment:
+   // const date = moment("Sep 21, 2017").format('YYYY/MM/DD');
+   // console.log(date); //2017/09/21
 
-  // Method 3- split
-  // let finalDate = "2013-03-10T02:00:00Z";
-  // console.log(finalDate.split("T")[0]);
-  //returns 2013-03-10
+   // Method 2 - using substring
+   // var finalDate = "2013-03-10T02:00:00Z";
+   // finalDate.substring(0, 10);
+   //returns "2013-03-10"
 
-  formatDate = (date) => {
-    let stringDate = JSON.stringify(date);
-    // console.log("date", date);
-    // console.log("stringDate", stringDate);
-    const lastDate = stringDate.substring(1, 10);
-    // console.log("splitlastDate", lastDate); // 2020/12/17
-    return lastDate;
-  };
+   // Method 3- split
+   // let finalDate = "2013-03-10T02:00:00Z";
+   // console.log(finalDate.split("T")[0]);
+   //returns 2013-03-10
 
-  render() {
-    console.log("this.formatDate", this.state.start_date);
-    return (
-      <div className="pageMission">
-        <div className="detailsMission">
-          <div className="detailsMissionBlock">
-            <h1 className="missionHeader">Détails de la mission</h1>
-            <p className="missionHeader">Statut {this.state.status}</p>
-            <h3 className="missionHeader">{this.state.expertise_required}</h3>
-            <h2 className="missionHeader">{this.state.title}</h2>
+   // formatDate = () => {
+   //   let finalDate=JSON.stringify(this.state.start_date);
+   //   console.log("finalDate",finalDate)
+   //   const lastDate=finalDate.substring(0, 10);;
+   //   //console.log("finaldate", finalDate.split("T")[0]) // 2020/12/17
+   // };
 
+   render() {
+      return (
+         <div className='pageMission'>
+            <div className='detailsMission'>
+               <div className='detailsMissionBlock'>
+                  <h1 className='missionHeader'>Détails de la mission</h1>
+                  <p className='missionHeader'>Statut {this.state.status}</p>
+                  <h3 className='missionHeader'>{this.state.expertise_required}</h3>
+                  <h2 className='missionHeader'>{this.state.title}</h2>
+
+                  <p>
+                     <GrLocation className='locationIcon' />
+                     <span> Lieu :</span> {this.state.location}
+                  </p>
+                  <p>
+                     <span>Secteur :</span> {this.state.sector}
+                  </p>
+                  <p>
+                     <span>Date de début :</span>
+                     {this.state.start_date}
+                     {/* {this.formatDate(this.state.start_date)} */}
+                  </p>
+                  <p>
+                     <span>Date de fin :</span> {this.state.end_date}
+                  </p>
+                  <p>
+                     <span>Rythme :</span> {this.state.availability_frequency}
+                  </p>
+                  <p>
+                     <span>Compétences requises :</span> {this.state.requiredSkills}
+                  </p>
+                  <p>
+                     <span>Description :</span> {this.state.description}
+                  </p>
+                  <button className='buttonHelp'>Proposer son aide </button>
+               </div>
+            </div>
+            <div>{this.renderEditForm()} </div>
             <p>
-              <GrLocation className="locationIcon" />
-              <span> Lieu :</span> {this.state.location}
+               <button className='deleteButton' onClick={() => this.deleteMission()}>
+                  Supprimer la mission
+               </button>
             </p>
-            <p>
-              <span>Secteur :</span> {this.state.sector}
-            </p>
-            <p>
-              <span>Date de début : </span>
-              {this.state.start_date}
-              {/* {this.formatDate(this.state.start_date)} */}
-            </p>
-            <p>
-              <span>Date de fin : </span>
-              {this.state.end_date}
-              {/* {this.formatDate(this.state.end_date)} */}
-            </p>
-            <p>
-              <span>Rythme :</span> {this.state.availability_frequency}
-            </p>
-            <p>
-              <span>Compétences requises :</span> {this.state.requiredSkills}
-            </p>
-            <p>
-              <span>Description :</span> {this.state.description}
-            </p>
-            <button className="buttonHelp">Proposer son aide </button>
-          </div>
-        </div>
 
-        <Link className="redirect_link" to={"/missions"}>
-          Retour à la liste des missions
-        </Link>
-      </div>
-    );
-  }
+            {/* {this.props.loggedInUser && 
+            (<div>
+              <div>{this.renderEditForm()} </div>
+              <p>
+              <button onClick={() => this.deleteMission()}>Supprimer la mission</button>
+              </p>
+            </div>)
+          } */}
+
+            <Link className='redirect_link' to={'/missions'}>
+               Retour à la liste des missions
+            </Link>
+         </div>
+      );
+   }
 }
 
 export default MissionDetails;
--- projet3-client/src/components/monEspace/Profile.js
+++ projet3-client/src/components/monEspace/Profile.js
@@ -1,50 +1,50 @@
-import React from "react";
-import { HiOutlineLocationMarker } from "react-icons/hi";
-import { FaRegEdit } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { FaRegEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Profile = (props) => {
-  const formatDate = (date) => {
-    date = new Date(date);
-    return date.toDateString();
-  };
-  return (
-    <div className="profile">
-      <div className="user_details">
-        <div>
-          <HiOutlineLocationMarker size={40} color="white" />{" "}
-          <p>{props.user.location}</p>
-        </div>
-        <ul>
-          <li>
-            <p>Inscription faite le </p>{" "}
-            <p>{formatDate(props.user.createdAt)}</p>
-          </li>
-          <li>
-            <p>Nombre d'annonces postées depuis l'inscription</p>
-            <p>{props.missions.length}</p>
-          </li>
-          <li>
-            <p>Email</p> <p>{props.user.email}</p>
-          </li>
-        </ul>
-        <Link to="">
-          <FaRegEdit size={40} />
-        </Link>
-        <ul>
-          <li>
-            <p>Mot de passe</p> <p>*************</p>
-          </li>
-          <li>
-            <p>Cause défendue</p> <p>{props.user.cause}</p>
-          </li>
-        </ul>
-        <div className="description">
-          <p>Description</p> <p>{props.user.description}</p>
-        </div>
+   const formatDate = (date) => {
+      date = new Date(date);
+      return date.toDateString();
+   };
+
+   return (
+      <div className='profile'>
+         <div className='user_details'>
+            <div>
+               <HiOutlineLocationMarker size={40} color='white' /> <p>{props.user.location}</p>
+            </div>
+            <ul>
+               <li>
+                  <p>Inscription faite le </p>
+                  <p>{formatDate(props.user.createdAt)}</p>
+               </li>
+               <li>
+                  <p>Nombre d'annonces postées depuis l'inscription</p>
+                  <p>{props.missions.length}</p>
+               </li>
+               <li>
+                  <p>Email</p> <p>{props.user.email}</p>
+               </li>
+            </ul>
+            <Link to={`/users/${props.user._id}/edit`}>
+               <FaRegEdit size={40} />
+            </Link>
+            <ul>
+               <li>
+                  <p>Mot de passe</p> <p>*************</p>
+               </li>
+               <li>
+                  <p>Cause défendue</p> <p>{props.user.cause}</p>
+               </li>
+            </ul>
+            <div className='description'>
+               <p>Description</p> <p>{props.user.description}</p>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Profile;
